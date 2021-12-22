167a754260dec05aeb4cc9073adadf92328d5f0b
arkhilena <94968252+arkhilena@users.noreply.github.com>
2021-12-22 14:31:18 +0300

    Add files via upload

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'second',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: ['@nuxtjs/style-resources', 'nuxt-gsap-module'],
+
+  gsap: {
+    extraPlugins: {
+      scrollTrigger: true
+    }
+  }, 
+
+
+  styleResources: {
+    scss: [
+      './assets/scss/*.scss',
+      ]
+  },
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+  ],
+  // bootstrapVue: {
+  //     bootstrapVueCSS: false,
+  //     bootstrapCSS: false,
+  //     // components: ['BContainer', 'BRow', 'BCol', 'BButton', 'BLink'],
+  //     // componentPlugins: ['BVToastPlugin'],
+  // },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+    transpile: [
+      "gsap"
+    ]
+  }
+}
